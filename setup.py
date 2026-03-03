90a6cd1b6c6132da4d065dbdec706f728953910a
Krotov <krotov@example.com>
2026-03-03 11:10:39 +0300

    Добавлен первый узел: вывод Hello ROS 2 World

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'krotov'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ldps-06',
+    maintainer_email='ldps-06@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'first_node = krotov.first_node:main'
+        ],
+    },
+)
